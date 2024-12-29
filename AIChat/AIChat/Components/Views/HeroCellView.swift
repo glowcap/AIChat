d52a905b7af3190fa9084537a12d7717328b456a
daymein <daymeing@gmail.com>
2024-12-29 09:34:55 -0600

    [Feature] HeroCellView
    - added HeroCellView

---
+++ AIChat/AIChat/Components/Views/HeroCellView.swift
@@ -0,0 +1,109 @@
+//
+//  HeroCellView.swift
+//  AIChat
+//
+//  Created by Daymein Gregorio on 12/29/24.
+//
+
+import SwiftUI
+
+struct HeroCellView: View {
+
+  var title: String? = "This is some title"
+  var subtitle: String? = "This is some subtitle that will go here"
+  var imageName: String? = Constants.randomImage
+
+  var body: some View {
+    cellBackground
+      .cornerRadius(16)
+  }
+
+}
+
+// MARK: - Subviews
+
+private extension HeroCellView {
+
+  var cellBackground: some View {
+    ZStack {
+      if let imageName {
+        ImageLoaderView(urlString: imageName)
+      } else {
+        Rectangle()
+          .fill(.accent)
+      }
+    }
+    .overlay(
+      alignment: .bottomLeading,
+      content: { textContent }
+    )
+  }
+
+  var textContent: some View {
+    VStack(alignment: .leading, spacing: 4) {
+      if let title {
+        Text(title)
+          .font(.headline)
+      }
+      if let subtitle {
+        Text(subtitle)
+          .font(.subheadline)
+      }
+    }
+    .foregroundStyle(.white)
+    .padding(16)
+    .frame(maxWidth: .infinity, alignment: .leading)
+    .background(textBackgroundGradient)
+  }
+
+  var textBackgroundGradient: some View {
+    LinearGradient(
+      colors: [
+        Color.black.opacity(0),
+        Color.black.opacity(0.3),
+        Color.black.opacity(0.6)
+      ],
+      startPoint: .top,
+      endPoint: .bottom
+    )
+  }
+
+}
+
+#Preview("Content Options") {
+  ScrollView {
+    VStack { // pushes scroll bar to screen edge
+      HeroCellView()
+        .frame(width: 300, height: 200)
+
+      HeroCellView(imageName: nil)
+        .frame(width: 300, height: 200)
+
+      HeroCellView(title: nil)
+        .frame(width: 300, height: 200)
+
+      HeroCellView(subtitle: nil)
+        .frame(width: 300, height: 200)
+    }
+    .frame(maxWidth: .infinity)
+  }
+}
+
+#Preview("Size Variations") {
+  ScrollView {
+    VStack {
+      HeroCellView()
+        .frame(width: 300, height: 200)
+
+      HeroCellView()
+        .frame(width: 300, height: 400)
+
+      HeroCellView()
+        .frame(width: 200, height: 200)
+
+      HeroCellView()
+        .frame(width: 200, height: 400)
+    }
+    .frame(maxWidth: .infinity)
+  }
+}
--- README.md
+++ README.md
@@ -5,7 +5,7 @@ AIChat allows users to generate AI avatars to chat with.
 ## Branching
 Branches are broken into sections and work done. Naming follows sXXpXX (ie. Section 01 Part: 01). Use the following to reference specific branches 
 
-### Section 1: Project Setup 
+### Module 1: Project Setup 
 
 1. Project Settings, Github, App Icon, & Launch Screen
 2. Adding SwiftLint
@@ -13,11 +13,15 @@ Branches are broken into sections and work done. Naming follows sXXpXX (ie. Sect
 4. Setup TabBar and NavigationStacks
 5. Adding root App State to the Environment
 
-### Section 2: Onboarding Flow
+### Module 2: Onboarding Flow
 
 1. WelcomeView & ImageLoaderView
 2. OnboardingIntroView & OnboardingColorView
 3. OnboardingCompletedView
+
+### Module 3: TabBar Flow
+
+1. ExploreView: HeroCellView
 
 ## Things to do
 
